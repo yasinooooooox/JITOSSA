3c0ee646d539715a9756a676baddfa2355d5ca20
Omar <165955792+Omarcharaf1@users.noreply.github.com>
2024-04-11 00:14:52 +0000

    Update ai-toon.js

--- plugins/ai-toon.js
+++ plugins/ai-toon.js
@@ -1,20 +1,20 @@
 import fetch from 'node-fetch';
 
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
-    if (!text || !args[0]) throw `> Usage: ${usedPrefix + command} <aversion> <text>\n\nAvailable verions:\nai\nv1\nv2\nv3\nv4\nv5\nv6\n\nExample: ${usedPrefix + command} v4 cute girl in pink dress`;
+    if (!text || !args[0]) throw `> حاول أن تقوم بإرسال المستويات او v2 في كل طلب مع كتابة مايدور في مخيلتك \n: ${usedPrefix + command} <aversion> <text>\n\nAvailable verions:\nai\nv1\nv2\nv3\nv4\nv5\nv6\n\nExample: ${usedPrefix + command} v4 cute girl in pink dress`;
 
     const apiVersion = args[0].toLowerCase();
     const validVersions = ['ai', 'v1', 'v2', 'v3', 'v4', 'v5', 'v6'];
 
     if (!validVersions.includes(apiVersion)) {
-        throw `> Invalid version. Supported versions: ${validVersions.join(', ')}`;
+        throw `لايقبل بهاذا المستوى حاول بأحد المستويات المتوفرة: ${validVersions.join(', ')}`;
         m.react('🤐');
     }
 
     const promptText = args.slice(1).join(' ');
 
     try {
-        let mess = await m.reply('> Generating toon image...');
+        let mess = await m.reply(waitt);
         m.react('🖌');
 
         const endpoint = `https://aemt.me/${apiVersion}/text2img?text=${encodeURIComponent(promptText)}`;
@@ -24,20 +24,20 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
             const imageBuffer = await response.arrayBuffer();
 
             //await m.reply({ key: mess.key, text: '> Here generated image...' });
-            await conn.sendFile(m.chat, Buffer.from(imageBuffer), 'toon_image.png', null, m);
+            await conn.sendFile(m.chat, Buffer.from(imageBuffer), 'toon_image.png', `تبعني ياصديقي 😆\n _*instagram.com/ovmar_1*_`, m);
             await m.react('😊');
         } else {
-            throw '> Toon image generation failed';
+            throw '> خطأ في توليد الصورة حاول لاحقا 🙁';
             m.react('😕');
         }
     } catch {
-        throw '> Oops! Something went wrong while generating toon image. Please try again later.';
+        throw 'حدث حطأ جاول لاحقا 🙁';
         m.react('😕');
     }
 };
 
 handler.help = ['toonai <version> <text>'];
-handler.tags = ['ai'];
+handler.tags = ['drawing'];
 handler.command = ['toonai', 'toonimage', 'toon'];
 
 export default handler;
