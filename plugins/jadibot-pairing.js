e17b244e3cb38c85392a9b87044033d9dce1e8a9
Omar <165955792+Omarcharaf1@users.noreply.github.com>
2024-04-12 12:19:41 +0000

    Update jadibot-pairing.js

--- plugins/jadibot-pairing.js
+++ plugins/jadibot-pairing.js
@@ -1,146 +1,100 @@
-import {
-    useMultiFileAuthState,
-    fetchLatestBaileysVersion,
-    makeCacheableSignalKeyStore,
-    PHONENUMBER_MCC,
-    DisconnectReason
-} from "@whiskeysockets/baileys";
+// TODO: fix jadibot
+var qrcode = require('qrcode');
+var ws = require('ws');
 
-import {
-    makeWaSocket
-} from '../../lib/simple.js';
-import path from 'path';
-import {
-    fileURLToPath,
-    pathToFileURL
-} from 'url';
-import fs from "fs";
-import pino from "pino";
-import NodeCache from "node-cache";
-import yargs from 'yargs';
-let isInit = false;
-const handler = async (m, {
-    conn,
-    text,
-    usedPrefix,
-    command
+var {
+	DisconnectReason
+} = require('@adiwajshing/baileys');
+
+var handler = async (m, {
+	conn: _conn,
+	args,
+	usedPrefix,
+	command,
+	isOwner
 }) => {
-    if (!text) {
-        return m.reply(`Masukkan Nomor!\n\nContoh: *${usedPrefix + command}* ${m.sender.split("@")[0]}`);
-    }
+	/** @type {import('../lib/connection').Socket} */
+	var Connection = (await import('../lib/connection.js')).default;
+	var Store = (await import('../lib/store.js')).default;
+	var parent = args[0] && args[0] == 'plz' ? _conn : await Connection.conn
+	if (!((args[0] && args[0] == 'plz') || (await Connection.conn).user.jid == _conn.user.jid)) {
+		throw 'Tidak bisa membuat bot didalam bot!\n\nhttps://wa.me/' + (await Connection.conn).user.jid.split`@` [0] + '?text=.jadibot'
+	}
 
-    if (!conn.user.jid) {
-        return m.reply(`Tidak dapat membuat *Jadibot* pada ${conn.user.jid.split("@")[0]}`);
-    }
+	var id = Connection.conns.size
+	var auth = Store.useMemoryAuthState()
+	var store = Store.makeInMemoryStore()
+	var conn = await Connection.start(null, {
+		isChild: true,
+		connectionOptions: {
+			auth: auth.state
+		},
+		store
+	})
+	var logout = async () => {
+		await parent.sendMessage(conn.user?.jid || m.chat, {
+			text: 'Koneksi terputus...'
+		})
+		try {
+			conn.ws.close()
+		} catch {}
+		Connection.conns.delete(id)
+	}
+	var lastQr, shouldSendLogin, errorCount = 0
+	conn.ev.on('connection.update', async ({
+		qr,
+		isNewLogin,
+		lastDisconnect
+	}) => {
+		if (shouldSendLogin && conn.user) {
+			await parent.sendMessage(conn.user.jid, {
+				text: 'Berhasil tersambung dengan WhatsApp - mu.\n*NOTE: Ini cuma numpang*\n' + JSON.stringify(conn.user, null, 2)
+			}, {
+				quoted: m
+			})
+		}
+		if (qr) {
+			if (lastQr) lastQr.delete()
+			lastQr = await parent.sendFile(m.chat, await qrcode.toDataURL(qr, {
+				scale: 8
+			}), 'qrcode.png', `
+Scan QR ini untuk jadi bot sementara
+1. Klik titik tiga di pojok kanan atas
+2. Ketuk perangkat tertaut
+3. Scan QR ini 
 
-    const __dirname = await global.__dirname(process.argv[1]);
+QR akan Expired !
+`.trim(), m)
+		}
+		if (isNewLogin)
+			shouldSendLogin = true
 
-    const {
-        state,
-        saveCreds
-    } = await useMultiFileAuthState("jadibot/" + m.sender.split("@")[0]);
-    const {
-        version
-    } = await fetchLatestBaileysVersion();
-    const msgRetryCounterCache = new NodeCache();
+		if (lastDisconnect) {
+			var code = lastDisconnect?.error?.output?.statusCode || lastDisconnect?.error?.output?.payload?.statusCode
+			if (code && code !== DisconnectReason.loggedOut && conn?.ws.readyState !== ws.CONNECTING) {
+				console.log(await Connection.reload(conn, true, {
+					isChild: true
+				}).catch(console.error))
+			} else if (code == DisconnectReason.loggedOut)
+				logout()
+			errorCount++;
+		}
 
-    const config = {
-        printQRInTerminal: false,
-        mobile: false,
-        version,
-        browser: ["Ubuntu", "Chrome", "20.0.04"],
-        markOnlineOnConnect: true,
-        generateHighQualityLinkPreview: true,
-        msgRetryCounterCache,
-        defaultQueryTimeoutMs: undefined,
-        logger: pino({
-            level: 'fatal'
-        }),
-        auth: {
-            creds: state.creds,
-            keys: makeCacheableSignalKeyStore(state.keys, pino().child({
-                level: 'silent',
-                stream: 'store'
-            })),
-        },
-    };
+		if (errorCount > 5)
+			logout()
 
-    global.connection = makeWaSocket(config);
+	})
 
-    connection.ev.on('connection.update', async (update) => {
-        const {
-            connection,
-            lastDisconnect
-        } = update;
-        if (connection === 'connecting') {
-            conn.reply(m.chat, `Menghubungkan dengan Jadibot...\n*${wait}*`, m);
-        } else if (connection === 'open') {
-            conn.reply(m.chat, `Terhubung dengan *Jadibot*\n\n• Peserta: *@${m.sender.split("@")[0]}*`, m);
-        } else if (connection === 'close') {
-            conn.reply(m.chat, `Tidak terhubung dengan *Jadibot*.`, m);
-        }
+	Connection.conns.set(id, conn)
+}
 
-        if (lastDisconnect && lastDisconnect.error && lastDisconnect.error.output && lastDisconnect.error.output.statusCode !== DisconnectReason.loggedOut) {
-            console.log(await reloadHandler(true));
-        }
-    });
 
-    async function reloadHandler(restartConn) {
-        let Handler = await import('../../handler.js');
-        let handler = Handler;
-        if (restartConn) {
-            try {
-                connection.ws.close();
-            } catch {}
-            global.connection = {
-                ...connection,
-                ...makeWaSocket(config)
-            };
-            if (!isInit) {
-                connection.ev.off('messages.upsert', connection.handler);
-                connection.ev.off('group-participants.update', connection.participantsUpdate);
-                connection.ev.off('message.delete', connection.onDelete);
-                connection.ev.off('connection.update', connection.connectionUpdate);
-                connection.ev.off('creds.update', connection.credsUpdate);
-            }
+handler.help = ['jadibot']
+handler.tags = ['jadibot']
 
-            connection.handler = handler.handler.bind(global.connection);
-            connection.participantsUpdate = handler.participantsUpdate.bind(global.connection);
-            connection.onDelete = handler.deleteUpdate.bind(global.connection);
-            connection.connectionUpdate = connectionUpdate.bind(global.connection);
-            connection.credsUpdate = saveCreds.bind(global.connection);
+handler.command = /^jadibot$/i
 
-            connection.ev.on('messages.upsert', connection.handler);
-            connection.ev.on('group-participants.update', connection.participantsUpdate);
-            connection.ev.on('message.delete', connection.onDelete);
-            connection.ev.on('connection.update', connection.connectionUpdate);
-            connection.ev.on('creds.update', connection.credsUpdate);
-            isInit = false;
-            return true;
-        }
-    }
+handler.disabled = true
+handler.limit = true
 
-    if (!connection.authState.creds.registered) {
-        setTimeout(async () => {
-            let phoneNumber = text.replace(/[^0-9]/g, '');
-            if (!Object.keys(PHONENUMBER_MCC).some(v => phoneNumber.startsWith(v))) {
-                phoneNumber = text.replace(/[^0-9]/g, '');
-            }
-            try {
-                const code = await connection.requestPairingCode(phoneNumber);
-                const yourCode = code?.match(/.{1,4}/g)?.join("-") || code;
-                const msgReply = await conn.reply(m.chat, '```Masukkan kode dibawah ini untuk jadi bot sementara\n\n1. Klik titik tiga di pojok kanan atas\n2. Ketuk perangkat tertaut\n3. Ketuk tautkan perangkat\n4. Ketuk tautkan dengan nomer telepon saja\n5. Masukkan kode di bawah ini\n\nNote: kode dapat expired kapan saja!```', m);
-                await conn.reply(m.chat, yourCode, msgReply);
-            } catch (error) {
-                console.error('Error requesting pairing code:', error);
-                conn.reply(m.chat, 'Gagal membuat bot sementara, coba lagi nanti.', m);
-            }
-        }, 3000);
-    }
-    reloadHandler();
-};
-
-handler.help = ["pairing"];
-handler.tags = ["owner"];
-handler.command = /^(pairing)$/i;
-export default handler;
+module.exports = handler
