e3b4b313f0cb699ee4e3c29dc6b6ccb61b7abd1e
Omar <165955792+Omarcharaf1@users.noreply.github.com>
2024-04-12 21:39:59 +0000

    Update jitossaai.js

--- plugins/jitossaai.js
+++ plugins/jitossaai.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+let smartModeEnabled = true; // تعيين الوضع الذكي كافتراضياً عند تشغيل البوت
+let enabledUsers = {}; // متغير لتخزين حالة تفعيل الوضع الذكي للمستخدمين
+
 let handler = async (m, { conn, text }) => {
   conn.autoai = conn.autoai ? conn.autoai : {};
 
@@ -7,10 +10,19 @@ let handler = async (m, { conn, text }) => {
     throw `*يمكنك الآن التحدث مع الذكاء الاصطناعي مباشرة بدون أوامر. يمكنك تفعيل الوضع الذكي عبر .autoai on وإلغاء الوضع الذكي عبر .autoai off*`;
   }
 
+  // التحقق من تفعيل الوضع الذكي عند تشغيل البوت
+  if (smartModeEnabled && !enabledUsers[m.sender]) {
+    enabledUsers[m.sender] = true; // تفعيل الوضع الذكي للمستخدم
+    conn.autoai[m.sender] = { pesan: [] };
+    m.reply("[ ✓ ] تم الانتقال بنجاح للوضع الذكي للبوت إسألني أي سؤال وسوف أجيبك لا تتردد يا صديقي 😉");
+  }
+
   if (text == "on") {
+    enabledUsers[m.sender] = true; // تفعيل الوضع الذكي للمستخدم
     conn.autoai[m.sender] = { pesan: [] };
     m.reply("[ ✓ ] تم الانتقال بنجاح للوضع الذكي للبوت إسألني أي سؤال وسوف أجيبك لا تتردد يا صديقي 😉");
   } else if (text == "off") {
+    delete enabledUsers[m.sender]; // إلغاء تفعيل الوضع الذكي للمستخدم
     delete conn.autoai[m.sender];
     m.reply("[ ✓ ] تم بنجاح الرجوع للوضع العادي للبوت");
   } else {
@@ -45,7 +57,7 @@ handler.before = async (m, { conn }) => {
   if (m.isBaileys && m.fromMe) return;
   if (!m.text) return;
 
-  if (!conn.autoai[m.sender]) return;
+  if (!enabledUsers[m.sender]) return;
 
   if (
     m.text.startsWith(".") ||
