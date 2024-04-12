4fa480c6ef2b703c9a20b4ed42a8827b46ad42c8
Omar <165955792+Omarcharaf1@users.noreply.github.com>
2024-04-12 11:12:09 +0000

    Update herramientas-hd.js

--- plugins/herramientas-hd.js
+++ plugins/herramientas-hd.js
@@ -1,46 +1,55 @@
-import FormData from "form-data";
-import Jimp from "jimp";
-const handler = async (m, {conn, usedPrefix, command}) => {
- try {    
-  let q = m.quoted ? m.quoted : m;
-  let mime = (q.msg || q).mimetype || q.mediaType || "";
-  if (!mime) throw `هاذا الأمر خاص بتحسين جودة الصورة قم باإرسال الصور ثم الرد\n\n ${usedPrefix + command}`;
-  if (!/image\/(jpe?g|png)/.test(mime)) throw `حدث خطأ..`;
-  m.reply("wait");
-  let img = await q.download?.();
-  let pr = await remini(img, "enhance");
-  conn.sendMessage(m.chat, {image: pr}, {quoted: m});
- } catch {
-  throw "هنالك مشكلة في هاذا الأمر حاول مرة اخرى...";
- }
-};
-handler.help = ["remini", "hd", "enhance"];
-handler.tags = ["image-edit"];
-handler.command = ["remini", "hd", "enhance"];
-export default handler;
+import {
+    VyroAI
+} from "../../lib/ai/vyro-ai.js";
+import {
+    ImageProcessor
+} from "../../lib/tools/art-enhance.js";
 
-async function remini(imageData, operation) {
-  return new Promise(async (resolve, reject) => {
-    const availableOperations = ["enhance", "recolor", "dehaze"];
-    if (availableOperations.includes(operation)) {
-      operation = operation;
+const genAi = new VyroAI({
+    link: false,
+    buffer: true
+});
+const generate = new ImageProcessor({
+    token: '',
+    replicate: "3a4886dd3230e523600d3b555f651dc82aba3a4e"
+});
+
+let handler = async (m, {
+    conn,
+    args,
+    usedPrefix,
+    command
+}) => {
+    let q = m.quoted ? m.quoted : m;
+    let mime = (q.msg || q).mimetype || q.mediaType || '';
+
+    if (/image/g.test(mime) && !/webp/g.test(mime)) {
+        try {
+            let media = await q.download();
+            let sauce = await genAi.upscaleImage(media);
+            await conn.sendFile(m.chat, sauce.buffer, null, '', m);
+        } catch (e) {
+            try {
+                let media = await q.download();
+                let sauce = await generate.upscaleImage(media);
+                await conn.sendFile(m.chat, sauce, null, '', m);
+            } catch (e) {
+                try {
+                    let media = await q.download();
+                    let sauce = await generate.artEnhance(media);
+                    await conn.sendFile(m.chat, sauce, null, '_ثم تحسين الجودة_ \n _*instagram.com/ovmar_1*_', m);
+                } catch (e) {
+                    await m.reply("خطأ أثناء تحسين جودة الصورة");
+                }
+            }
+        }
     } else {
-      operation = availableOperations[0];
+        await m.reply(`يرجى إرسال صورة مع تسمية *${usedPrefix + command}* أو علامة على الصورة التي تم إرسالها مسبقًا`);
     }
-    const baseUrl = "https://inferenceengine.vyro.ai/" + operation + ".vyro";
-    const formData = new FormData();
-    formData.append("image", Buffer.from(imageData), {filename: "enhance_image_body.jpg", contentType: "image/jpeg"});
-    formData.append("model_version", 1, {"Content-Transfer-Encoding": "binary", contentType: "multipart/form-data; charset=utf-8"});
-    formData.submit({url: baseUrl, host: "inferenceengine.vyro.ai", path: "/" + operation, protocol: "https:", headers: {"User-Agent": "okhttp/4.9.3", Connection: "Keep-Alive", "Accept-Encoding": "gzip"}},
-      function (err, res) {
-        if (err) reject(err);
-        const chunks = [];
-        res.on("data", function (chunk) {chunks.push(chunk)});
-        res.on("end", function () {resolve(Buffer.concat(chunks))});
-        res.on("error", function (err) {
-        reject(err);
-        });
-      },
-    );
-  });
-}
+};
+
+handler.help = ['hd', 'enhance'];
+handler.tags = ['image-edit'];
+handler.command = /^(hd|enhance)$/i;
+
+export default handler;
