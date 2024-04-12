efd68d76b2096adb48828bd1c09e1b691935aade
Omar <165955792+Omarcharaf1@users.noreply.github.com>
2024-04-12 08:50:13 +0000

    Update yt-ytv.js

--- plugins/yt-ytv.js
+++ plugins/yt-ytv.js
@@ -1,61 +1,51 @@
-// Update by Xnuvers007
+import ytdl from "ytdl-core";
 
-import { youtubedlv2, youtubedl } from '@bochilteam/scraper'
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+	if (!text) return m.reply(`*Example:* .${command} https://www.youtube.com/xxxxxxx`);
+	conn.sendMessage(m.chat, { react: { text: '🕒', key: m.key }})
+	let obj = await ytmp3(text);
+	let title = obj.meta.title;
+	conn.sendFile(m.chat, obj.buffer, '', "", m, 0, {
+		mimetype: "video/mp4",
+		fileName: `${title}.mp4`,
+		asDocument: false,
+	});
+};
 
-const handler = async (m, { conn, args, command }) => {
-  if (!args[0]) throw 'Where`s Url?' // Zod
-  const v = args[0]
-
-  const resolutions = ["144p", "240p", "360p", "480p", "720p", "1080p"]
-  let qu = args[1] && resolutions.includes(args[1]) ? args[1] : "360p"
-  let q = qu.replace('p', '')
-
-  let thumb = {}
-  try {
-    const thumb2 = yt.thumbnails[0].url
-    thumb = { jpegThumbnail: thumb2 }
-  } catch (e) {}
-
-  let yt
-  try {
-    yt = await youtubedl(v)
-  } catch {
-    yt = await youtubedlv2(v)
-  }
-
-  const title = await yt.title
-
-  let size = ''
-  let dlUrl = ''
-  let selectedResolution = ''
-  let selectedQuality = ''
-  for (let i = resolutions.length - 1; i >= 0; i--) {
-    const res = resolutions[i]
-    if (yt.video[res]) {
-      selectedResolution = res
-      selectedQuality = res.replace('p', '')
-      size = await yt.video[res].fileSizeH
-      dlUrl = await yt.video[res].download()
-      break
-    }
-  }
-
-  if (dlUrl) {
-    await m.reply(`Permintaan download video YouTube. Sedang diproses, mohon bersabar...`)
-
-    await conn.sendMessage(m.chat, { video: { url: dlUrl, caption: title, ...thumb } }, { quoted: m })
-
-    await m.reply(`▢ Title: ${title}
-▢ Resolution: ${selectedResolution}
-▢ Size: ${size}
-▢ Video telah berhasil diunduh!`)
-  } else {
-    await m.reply(`Maaf, video tidak tersedia untuk diunduh.`)
-  }
-}
-
-handler.command = /^(getvid|ytmp4|youtubemp4)$/i
-handler.help = ["ytmp4 <linkYt>","ytmp4 <linkYT>"]
+handler.help = ['ytmp4']
 handler.tags = ['downloader']
+handler.command = ['ytmp4', 'ytv']
+export default handler 
 
-export default handler
+async function ytmp3(url) {
+	try {
+		const { videoDetails } = await ytdl.getInfo(url, {
+			lang: "id",
+		});
+		const stream = ytdl(url, {
+			filter: "videoandaudio",
+		});
+		const chunks = [];
+		stream.on("data", (chunk) => {
+			chunks.push(chunk);
+		});
+		await new Promise((resolve, reject) => {
+			stream.on("end", resolve);
+			stream.on("error", reject);
+		});
+		const buffer = Buffer.concat(chunks);
+		return {
+			meta: {
+				title: videoDetails.title,
+				channel: videoDetails.author.name,
+				seconds: videoDetails.lengthSeconds,
+				description: videoDetails.description,
+				image: videoDetails.thumbnails.slice(-1)[0].url,
+			},
+			buffer: buffer,
+			size: buffer.length,
+		};
+	} catch (error) {
+		throw error;
+	}
+}
