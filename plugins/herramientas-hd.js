26ec3cd7579edb33d9bdf233c2b05fadbafbbe30
Omar <165955792+Omarcharaf1@users.noreply.github.com>
2024-04-12 11:27:38 +0000

    Update herramientas-hd.js

--- plugins/herramientas-hd.js
+++ plugins/herramientas-hd.js
@@ -1,88 +1,54 @@
+import uploadImage from '../lib/uploadImage.js'
+import deepai from 'deepai'
+import fetch from 'node-fetch'
 import FormData from 'form-data'
-import Jimp from 'jimp'
 
-var handler = async (m, { conn, usedPrefix, command }) => {
+deepai.setApiKey('04f02780-e0bd-44c1-afa2-14cf5a78948c')
 
-    conn.hdr = conn.hdr ? conn.hdr : {}
-    if (m.sender in conn.hdr)
-        return conn.reply(m.chat, `*🏴 هناك طلب قيد التنفيذ بالفعل، يرجى التحلي بالصبر*`, m, fake, )
-
+let handler = async (m, { conn, args, usedPrefix, command }) => {
     let q = m.quoted ? m.quoted : m
-    let mime = (q.msg || q).mimetype || q.mediaType || ""
-    if (!mime)
-        return conn.reply(m.chat, `*🎌 يرجى وضع علامة على الصورة*`, m, fake, )
-    if (!/image\/(jpe?g|png|mp4)/.test(mime))
-        return conn.reply(m.chat, `*🚩 نوع ${mime} غير مدعوم*`, m, fake, )
-    else
-        conn.hdr[m.sender] = true
-
-    conn.reply(m.chat, '⏰ يرجى الانتظار...', m, fake, )
-    let img = await q.download?.()
-
-    let error
-    try {
-        const result = await processing(img, "enhance")
-        conn.sendFile(m.chat, result, '', '🧃 *تم تحسين الصورة*\n\n' + cred.toString('utf-8'), m)
-    } catch (er) {
-        error = true
-    } finally {
-        if (error) {
-            return conn.reply(m.chat, `*🚩 حدث خطأ أثناء تحسين الصورة*`, m, fake, )
-        }
-        delete conn.hdr[m.sender]
+    let mime = (q.msg || q).mimetype || q.mediaType || ''
+    if (/image/g.test(mime) && !/webp/g.test(mime)) {
+    	try {
+			let img = await q.download?.()
+			let out = await uploadImage(img)
+			var resp = await deepai.callStandardApi("waifu2x", {
+                        image: out,
+                    })
+                    let w2x1 = resp['output_url']
+           var resep = await deepai.callStandardApi("waifu2x", {
+                        image: w2x1,
+                    })
+                    // console.log(resp);
+                    let w2x2 = resep['output_url']
+           var resup = await   deepai.callStandardApi("torch-srgan", {
+            image: w2x2,
+            })
+            await conn.sendFile(m.chat, resup['output_url'], 'simpcard.png', 'simp', m)
+    	} catch {
+    		await m.reply('Sedang membuat...')
+  let q = m.quoted ? m.quoted : m
+  let mime = (q.msg || q).mimetype || ''
+  if (!mime) throw 'Fotonya Mana?'
+  if (!/image\/(jpe?g|png)/.test(mime)) throw `Tipe ${mime} tidak didukung!`
+  let img = await q.download()
+  let body = new FormData
+  body.append('image', img, 'image')
+  let res = await fetch('http://max-image-resolution-enhancer.codait-prod-41208c73af8fca213512856c7a09db52-0000.us-east.containers.appdomain.cloud/model/predict', {
+    method: 'POST',
+    body
+  })
+  if (res.status !== 200) throw await res.json()
+  await conn.sendFile(m.chat, await res.buffer(), 'hd.jpg', 'Nihh,, Hade kan?', m)
+    	}
+    } else {
+    	m.reply(`Kirim gambar dengan caption *${usedPrefix + command}* atau tag gambar yang sudah dikirim`)
     }
 }
+handler.help = ['hd', 'enhance']
+handler.tags = ['tools']
+handler.command = /^(hd|enhance)$/i
 
-handler.help = ['hd']
-handler.tags = ['ai']
-handler.command = /^(hd)$/i
-
-handler.register = true
 handler.limit = true
 
 export default handler
-
-async function processing(urlPath, method) {
-    return new Promise(async (resolve, reject) => {
-        let Methods = ['enhance', 'recolor', 'dehaze']
-        method = Methods.includes(method) ? method : Methods[0]
-        let buffer,
-            Form = new FormData(),
-            scheme = 'https' + '://' + 'inferenceengine' + '.vyro' + '.ai/' + method
-        Form.append('model_version', 1, {
-            'Content-Transfer-Encoding': 'binary',
-            contentType: 'multipart/form-data; charset=utf-8',
-        })
-        Form.append('image', Buffer.from(urlPath), {
-            filename: 'enhance_image_body.jpg',
-            contentType: 'image/jpeg',
-        })
-        Form.submit(
-            {
-                url: scheme,
-                host: 'inferenceengine' + '.vyro' + '.ai',
-                path: '/' + method,
-                protocol: 'https:',
-                headers: {
-                    'User-Agent': 'okhttp/4.9.3',
-                    Connection: 'Keep-Alive',
-                    'Accept-Encoding': 'gzip',
-                },
-            },
-            function (err, res) {
-                if (err) reject()
-                let data = []
-                res
-                    .on('data', function (chunk, resp) {
-                        data.push(chunk)
-                    })
-                    .on('end', () => {
-                        resolve(Buffer.concat(data))
-                    })
-                res.on('error', (e) => {
-                    reject()
-                })
-            }
-        )
-    })
-}
