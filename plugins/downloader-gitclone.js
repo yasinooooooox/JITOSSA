c8f6777dce3172621b9dbee54cf05726c3955559
Omar <165955792+Omarcharaf1@users.noreply.github.com>
2024-04-12 09:23:22 +0000

    Update downloader-gitclone.js

--- plugins/downloader-gitclone.js
+++ plugins/downloader-gitclone.js
@@ -1,31 +1,18 @@
-import fetch from 'node-fetch';
-const regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i;
+const regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i
+var handler = async (m, { args, usedPrefix, command }) => {
+    if (!args[0]) throw `رابط النشروع Github الذي تريد تنزيله  \n ${usedPrefix}${command} https://github.com/omarcharaf1/JITOSSA`
+    if (!regex.test(args[0])) throw 'Url Tidak Valid! '
+    let [_, user, repo] = args[0].match(regex) || []
+    repo = repo.replace(/.git$/, '')
+    let url = `https://api.github.com/repos/${user}/${repo}/zipball`
+    let filename = (await fetch(url, { method: 'HEAD' })).headers.get('content-disposition').match(/attachment; filename=(.*)/)[1]
+    m.reply(wait)
+    conn.sendFile(m.chat, url, filename, null, m)
+}
+handler.help = ['gitclone'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+handler.command = /gitclone/i
 
-let handler = async (m, {
-    args,
-    usedPrefix,
-    command
-}) => {
-    try {
-        if (!args[0]) throw `Example user ${usedPrefix}${command} <url>`;
-        if (!regex.test(args[0])) throw 'Link salah!';
-        let [_, user, repo] = args[0].match(regex) || [];
-        repo = repo.replace(/.git$/, '');
-        let url = `https://api.github.com/repos/${user}/${repo}/zipball`;
-        await m.reply(wait);
-        let response = await fetch(url, {
-            method: 'HEAD'
-        });
-        let filename = response.headers.get('content-disposition').match(/attachment; filename=(.*)/)[1];
-        await conn.sendFile(m.chat, url, filename, null, m);
-    } catch (error) {
-        console.error('Error:', error.message);
-        m.reply(`Error: ${error.message}`);
-    }
-};
-handler.help = ['gitclone'].map(v => v + ' <url>');
-handler.tags = ['downloader'];
-handler.command = /^gitclone$/i;
-handler.limit = true;
+handler.limit = false
 
-export default handler;
+export default handler
