b0866a5809f7bebb71b71e0b5b0a6f071f1b76ba
Omar <165955792+Omarcharaf1@users.noreply.github.com>
2024-04-12 20:24:49 +0000

    Update jitossaai.js

--- plugins/jitossaai.js
+++ plugins/jitossaai.js
@@ -3,18 +3,14 @@ import axios from 'axios';
 let autoaiEnabled = {}; // قائمة لتخزين حالة الذكاء الاصطناعي لكل مستخدم
 
 export const handler = async (m, { conn, text }) => {
-  if (text && text.trim() === '.autobard on') {
-    autoaiEnabled[m.sender] = true;
-    m.reply("[ ✓ ] تم تفعيل وضع الذكاء الاصطناعي بنجاح. يمكنك الآن التحدث مباشرة مع الذكاء الاصطناعي.");
-    return; // للخروج من دالة المعالجة إذا تم تفعيل الوضع
-  } else if (text && text.trim() === '.autobard off') {
+  autoaiEnabled[m.sender] = true; // تفعيل وضع الذكاء الاصطناعي تلقائياً
+
+  if (text && text.trim() === '.autobard off') {
     autoaiEnabled[m.sender] = false;
     m.reply("[ ✓ ] تم إلغاء وضع الذكاء الاصطناعي بنجاح. يمكنك الآن استخدام الأوامر بشكل عادي.");
     return; // للخروج من دالة المعالجة إذا تم إلغاء الوضع
   }
 
-  if (!autoaiEnabled[m.sender]) return; // التحقق من إذا كان وضع الذكاء الاصطناعي مُفعّلاً أم لا
-  if (m.isBaileys && m.fromMe) return;
   if (!m.text) return;
 
   let name = conn.getName(m.sender);
