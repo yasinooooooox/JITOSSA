7e2d5ad5969e6913d5c4e582c80cde24865458d3
Omar <165955792+Omarcharaf1@users.noreply.github.com>
2024-04-12 09:31:45 +0000

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,36 +1,32 @@
-import { tiktokdl } from '@bochilteam/scraper';
-import fg from 'api-dylux';
+import fg from 'api-dylux' 
+import { tiktokdl } from '@bochilteam/scraper'
 
-let handler = async (m, { conn, text, args, usedPrefix, command }) => {
-  
- if (!args[0] && m.quoted && m.quoted.text) {
-  args[0] = m.quoted.text;
-}
-if (!args[0] && !m.quoted) throw `Give the link of the video Tiktok or quote a tiktok link`;
- if (!args[0].match(/tiktok/gi)) throw `Verify that the link is from TikTok`;
- 
- 
-  let txt = 'Here your Requested video';
-
-  try {
-    const { author: { nickname }, video, description } = await tiktokdl(args[0]);
-    const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd;
-    
-    if (!url) throw global.error;
-    
-    conn.sendFile(m.chat, url, 'tiktok.mp4', '', m);
-  } catch (err) {
-    try {
-      let p = await fg.tiktok(args[0]);
-      conn.sendFile(m.chat, p.play, 'tiktok.mp4', txt, m);
-    } catch {
-      m.reply('*An unexpected error occurred*');
-    }
-  }
-};
+let handler = async (m, { conn, text, args, usedPrefix, command}) => {
+if (!args[0]) throw `Enter the link of the video Tiktok`
+if (!args[0].match(/tiktok/gi)) throw `Verify that the link is from tiktok`
+let old = new Date()
+let txt = `∘  *Fetching* : ${((new Date - old) * 1)} ms`
+conn.reply(m.chat, global.wait, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
+title: '📤| downloads By Jessi-md 🌸',
+body: me,
+previewType: 0, thumbnail: thumb2, jpegThumbnail: thumb,
+sourceUrl: 'https://github.com/whiteshadowofficial' }}})
+try {
+let p = await fg.tiktok(args[0]) 
+conn.sendFile(m.chat, p.play, 'tiktok.mp4', txt, m)
+} catch {  	
+try { 
+const { author: { nickname }, video, description } = await tiktokdl(args[0])
+const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd
+if (!url) throw global.eror
+conn.sendFile(m.chat, url, 'fb.mp4', ``, m)
+} catch {
+m.reply('*☓ An unexpected error occurred*')
+}}}
 
 handler.help = ['tiktok'].map((v) => v + ' <url>');
 handler.tags = ['downloader'];
 handler.command = /^t(t|iktok(d(own(load(er)?)?|l))?|td(own(load(er)?)?|l))$/i;
 
-export default handler;
+export default handler
