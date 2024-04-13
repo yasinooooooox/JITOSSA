bcf8bb82f3fb980c39c4f211b091d70f6ed6b9db
Omar <165955792+Omarcharaf1@users.noreply.github.com>
2024-04-13 10:18:57 +0000

    Update jadibot-pairing.js

--- plugins/jadibot-pairing.js
+++ plugins/jadibot-pairing.js
@@ -1,185 +1,106 @@
-const { MessageType, useMultiFileAuthState, DisconnectReason, Browsers } = await import("@whiskeysockets/baileys")
-import qrcode from "qrcode"
-import fs from "fs"
-import P from 'pino';
-import * as ws from 'ws';
-const {child , spawn, exec} = await import('child_process');
-const { CONNECTING } = ws
-import { makeWaSocket, protoType, serialize } from '../lib/simple.js';
-
-let check1 = "NjBhZGVmZWI4N2M2"
-let check2 = "ZThkMmNkOGVlMDFmZD"
-let check3 = "UzYTI1MTQgIGluZ"
-let check4 = "m8tZG9uYXIuanMK"
-let check5 = "NzZjM2ZmMzU2MTEyMzM3OTczOWU5ZmFmMDZjYzUzO"
-let check6 = "DcgIF9hdXRvcmVzcG9uZGVyLmpzCjU5Yzc0ZjFjNmEz"
-let check8 = "NjNmYmJjYzA1YmFiY2MzZGU4MGRlICBpbmZvLWJvdC5qcwo"
-//
-let crm1 = "Y2QgcGx1Z2lucy"
-let crm2 = "A7IG1kNXN1b"
-let crm3 = "SBpbmZvLWRvbmFyLmpz"
-let crm4 = "IF9hdXRvcmVzcG9uZGVyLmpzIGluZm8tYm90Lmpz"
-let drm1 = "CkphZGlib3QsIEhlY2hv"
-let drm2 = "IHBvciBAQWlkZW5fTm90TG9naWM"
-let rtx = `*${wm}*
-*Langkah - Langkah!.*
-
-*Pindai kode QR ini untuk menjadi Bot (Langkah - Langkah!), Anda dapat menggunakan perangkat lain untuk memindai*
-
-*Langkah-langkah untuk memindai:*
-*1.- Ketuk tiga titik di sudut kanan atas di beranda WhatsApp Anda*
-*2.- Ketuk WhatsApp web atau perangkat yang sudah terhubung*
-*3.- Pindai kode QR ini*
-*Kode QR berlaku selama 60 detik!!*
-
-
-*Anda dapat mengirimkan ID yang diberikan ke bot secara pribadi untuk menghubungkan kembali bot tanpa harus memindai kode lagi, kode ini dimulai dengan /serbot.*
-*Ingatlah untuk keluar dari grup ketika Anda menjadi bot*
-
-Proses ini 100% Aman.`
+let baileys = require("@adiwajshing/baileys")
+let { useMultiFileAuthState, DisconnectReason, makeInMemoryStore, jidNormalizedUser, makeCacheableSignalKeyStore, PHONENUMBER_MCC } = baileys
+let { Boom } = require("@hapi/boom")
+let NodeCache = require("node-cache")
+let Pino = require("pino")
+let simple = require('../lib/simple')
+let fs = require('fs')
 
 if (global.conns instanceof Array) console.log()
 else global.conns = []
 
-let handler = async (m, { conn, args, usedPrefix, command, isOwner }) => {
-  let parentw = conn
-  if (conn.user.jid !== global.conn.user.jid) return parentw.reply(m.chat, 'Perintah ini hanya dapat digunakan di bot utama! wa.me/' + global.conn.user.jid.split`@`[0], m)
-  const comb = Buffer.from(crm1 + crm2 + crm3 + crm4, "base64")
-  exec(comb.toString("utf-8"), async (err, stdout, stderr) => {
-    const buffdr = Buffer.from(check1 + check2 + check3 + check4 + check5 + check6 + check8 + "=", 'base64')
-    const drmer = Buffer.from(drm1 + drm2, 'base64')
+let handler = async (m, { conn, args, usedPrefix, command, isOwner, text }) => {
+if (!text) throw m.reply('• *Example :* .jadibot 6288980870067')
+conn.reply(m.chat, '```Tunggu Sedang Menyiapkan Code Jadibot...```', m)
+let conns = global.conn
+let user = global.db.data.users[m.sender]
 
-    async function jddt() {
-      function randomString(length) {
-        var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz'.split('');
+    let authFile = 'lib/jadibot'+m.sender.split`@`[0]+'.data.json'
+    let isInit = !fs.existsSync(authFile)
+    let { state, saveCreds} = await useMultiFileAuthState(authFile)
+    let msgRetryCounterCache = new NodeCache() 
     
-        if (! length) {
-            length = Math.floor(Math.random() * chars.length);
-        }
-    
-        var str = '';
-        for (var i = 0; i < length; i++) {
-            str += chars[Math.floor(Math.random() * chars.length)];
-        }
-        return str;
-      }
- 
-      let uniqid = randomString(10)
-      if (!fs.existsSync("./jadibot/"+ uniqid)){
-          fs.mkdirSync("./jadibot/"+ uniqid, { recursive: false });
-      }
-      args[0] ? fs.writeFileSync("./jadibot/" + uniqid + "/creds.json", JSON.stringify(JSON.parse(Buffer.from(args[0], "base64").toString("utf-8")), null, '\t')) : ""
-      const { state, saveState, saveCreds } = await useMultiFileAuthState("./jadibot/" + uniqid)
-
-      const connectionOptions = {
-        printQRInTerminal: true,
-        auth: state,
-        logger: P({ level: 'silent'}),
-        browser: [`${namabot} Clone Bot`, 'Safari', '5.0'],
-      }
-
-      let conn = makeWaSocket(connectionOptions)
-      conn.isInit = false
-      let isInit = true
-
-      async function connectionUpdate(update) {
-        const { connection, lastDisconnect, isNewLogin, qr } = update
-        if (isNewLogin) conn.isInit = true
-        if (qr) parentw.sendMessage(m.chat, {image: await qrcode.toBuffer(qr, { scale: 8 }) , caption : rtx + drmer.toString("utf-8")}, { quoted: m })
-        const code = lastDisconnect?.error?.output?.statusCode || lastDisconnect?.error?.output?.payload?.statusCode
-        console.log(code)
-        if (code && code !== DisconnectReason.loggedOut && conn?.ws.readyState !== CONNECTING) {
-          let i = global.conns.indexOf(conn)
-          if (i < 0) return console.log(await creloadHandler(true).catch(console.error))
-          delete global.conns[i]
-          global.conns.splice(i, 1)
-          if (code !== DisconnectReason.connectionClosed){ parentw.sendMessage(m.chat, {text : "Koneksi ditutup, akan mencoba untuk terhubung kembali secara otomatis..."}, { quoted: m })
-          console.log(await creloadHandler(true).catch(console.error))}
-          else {
-              parentw.sendMessage(m.chat, {text : "Koneksi ditutup, Anda harus terhubung kembali secara manual..."}, { quoted: m })
-          }
-        }
-        if (global.db.data == null) loadDatabase()
-        if (connection == 'open') {
-        conn.isInit = true
-        global.conns.push(conn)
-        await parentw.sendMessage(m.chat, {text : args[0] ? "Berhasil terhubung ✅" : "*Berhasil terhubung✅* Dalam beberapa detik, kami akan mengirimkan ID yang harus Anda gunakan untuk terhubung kembali... Kirimkan ini ke bot secara pribadi ketika bot terputus."}, { quoted: m })
-        await sleep(5000)
-        if (!args[0]) parentw.sendMessage(m.chat, {text : usedPrefix + command + " " + Buffer.from(fs.readFileSync("./jadibot/" + uniqid + "/creds.json"), "utf-8").toString("base64")}, { quoted: m })
-        }
-      }
-
-      setInterval(async () => {
-        if (!conn.user) {
-          try { conn.ws.close() } catch { }
-          conn.ev.removeAllListeners()
-          let i = global.conns.indexOf(conn)
-          if (i < 0) return
-          delete global.conns[i]
-          global.conns.splice(i, 1)
-        }}, 60000)
-      
-      let handler = await import('../handler.js')
-      let creloadHandler = async function (restatConn) {
-        try {
-          const Handler = await import(`../handler.js?update=${Date.now()}`).catch(console.error)
-          if (Object.keys(Handler || {}).length) handler = Handler
-        } catch (e) {
-          console.error(e)
-        }
-        if (restatConn) {
-          try { conn.ws.close() } catch { }
-          conn.ev.removeAllListeners()
-          conn = makeWaSocket(connectionOptions)
-          isInit = true
-        }
-        if (!isInit) {
-          conn.ev.off('messages.upsert', conn.handler)
-          conn.ev.off('group-participants.update', conn.participantsUpdate)
-          // conn.ev.off('groups.update', conn.groupsUpdate)
-          conn.ev.off('message.delete', conn.onDelete)
-          conn.ev.off('connection.update', conn.connectionUpdate)
-          conn.ev.off('creds.update', conn.credsUpdate)
-        }
-
-        conn.welcome = '*┊↳🔰┊ Selamat datang di grup!!*\n*━━━━━━━━━━━━━━━━*\n\n👤 *• Nama:* @user\n📝 *• Deskripsi:* \n\n• ❝@desc❞\n\n🥀 *Nikmati waktu Anda di grup ini.*'
-        conn.bye = '*★════◈◈◈════★*\n┃• Selamat tinggal👋 *@user*\n┃• Pergilah dengan damai -*\n*★════◈◈◈════★*'
-        conn.spromote = '*┊↳🔰┊ @user Sekarang adalah Admin 👻*'
-        conn.sdemote = '*┊↳🔰┊ @user Tidak lagi menjadi Admin 〽️*'
-        conn.sDesc = '*┊↳🔰┊ Deskripsi grup telah diubah*\n\n*Deskripsi baru:* @desc'
-        conn.sSubject = '*┊↳🔰┊ Nama grup telah diubah*\n*Nama Baru:* @subject'
-        conn.sIcon = '*┊↳🔰┊ Gambar grup diperbarui!!*'
-        conn.sRevoke = '*┊↳🔰┊ Link grup telah diperbarui*\n*Link Baru:* @revoke'
-
-        conn.handler = handler.handler.bind(conn)
-        conn.participantsUpdate = handler.participantsUpdate.bind(conn)
-        // conn.groupsUpdate = handler.groupsUpdate.bind(conn)
-        conn.onDelete = handler.deleteUpdate.bind(conn)
-        conn.connectionUpdate = connectionUpdate.bind(conn)
-        conn.credsUpdate = saveCreds.bind(conn, true)
-
-        conn.ev.on('messages.upsert', conn.handler)
-        conn.ev.on('group-participants.update', conn.participantsUpdate)
-        // conn.ev.on('groups.update', conn.groupsUpdate)
-        conn.ev.on('message.delete', conn.onDelete)
-        conn.ev.on('connection.update', conn.connectionUpdate)
-        conn.ev.on('creds.update', conn.credsUpdate)
-        isInit = false
-        return true
-      }
-      creloadHandler(false)
+    const config = {
+    logger: Pino({ level: "fatal" }).child({ level: "fatal" }),
+    printQRInTerminal: false,
+    mobile: false,
+    auth: state,
+    browser: ["Ubuntu","Chrome","20.0.04"],
+    markOnlineOnConnect: true,
+    generateHighQualityLinkPreview: true,
+    msgRetryCounterCache,
+    defaultQueryTimeoutMs: undefined
     }
-    jddt()
-  })
+    conn = simple.makeWASocket(config)
+    let ev = conn.ev
+    
+     if (!conn.authState.creds.registered) {
+        setTimeout(async () => {
+          let codek = await conn.requestPairingCode(
+            parseInt(await m.sender.replace(/[^0-9]/g, ""))
+          );
+          codek = codek?.match(/.{1,4}/g)?.join("-") || codek;
+          conns.reply(text + '@s.whatsapp.net', codek, m)
+          console.log("code anda adalah: " + codek);
+        }, 3000);
+        }
+    async function connectionUpdate(update) {
+    const { connection, lastDisconnect } = update
+    console.log(update)
+    if (connection == 'connecting') {
+    console.log(connection)
+    } else if (connection == 'close') {
+    conns.reply(text + '@s.whatsapp.net', '```⏱️ koneksi terputus & mencoba menyambung ulang...```', m)
+    } else if (connection == 'open') {
+    conns.reply(text + '@s.whatsapp.net', '```✅ Tersambung```', m)
+    global.conns.push(conn)
+    }
+    if (lastDisconnect && lastDisconnect.error && lastDisconnect.error.output && lastDisconnect.error.output.statusCode !== DisconnectReason.loggedOut) {
+    console.log(reloadHandler(true))
+    }
+   }
+
+    reloadHandler = function (restatConn) {
+    let Handler = require('../handler')
+    let handler = require('../handler')
+    if (Object.keys(Handler || {}).length) handler = Handler
+    if (restatConn) {
+    try { conn.ws.close() } catch { }
+    conn = {
+      ...conn, ...simple.makeWASocket(config)
+     }
+   }
+   if (!isInit) {
+     conn.ev.off('messages.upsert', conn.handler)
+     conn.ev.off('group-participants.update', conn.onParticipantsUpdate)
+     conn.ev.off('connection.update', conn.connectionUpdate)
+     conn.ev.off('creds.update', conn.credsUpdate)
+   }
+
+   conn.welcome = 'Hai, @user!\nSelamat datang di grup *@subject*\n\n@desc'
+   conn.bye = 'Selamat tinggal @user!'
+   conn.spromote = '@user sekarang admin!'
+   conn.sdemote = '@user sekarang bukan admin!'
+   conn.handler = handler.handler.bind(conn)
+   conn.onParticipantsUpdate = handler.participantsUpdate.bind(conn)
+   conn.connectionUpdate = connectionUpdate.bind(conn)
+   conn.credsUpdate = saveCreds.bind(conn)
+
+   conn.ev.on('messages.upsert', conn.handler)
+   conn.ev.on('group-participants.update', conn.onParticipantsUpdate)
+   conn.ev.on('connection.update', conn.connectionUpdate)
+   conn.ev.on('creds.update', conn.credsUpdate)
+   isInit = false
+   return true
 }
-handler.help = ['jadibot']
+    reloadHandler()   
+}
+handler.help = ['jadibot *<number>*']
 handler.tags = ['jadibot']
-handler.command = /^(jadibot)/i
-
-handler.premium = true
-
-export default handler
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+handler.command = /^jadibot$/i
+handler.register = true
+handler.premium = false
+handler.limit = 100
+handler.owner = false
+handler.private = true
+module.exports = handler
