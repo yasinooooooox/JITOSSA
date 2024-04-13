fbed3ad53e122b093eb63f07fba5ed0376b97598
Omar <165955792+Omarcharaf1@users.noreply.github.com>
2024-04-13 11:48:06 +0000

    Update _owner.js

--- plugins/_owner.js
+++ plugins/_owner.js
@@ -1,8 +1,33 @@
-function handler(m) {
-  let data = global.owner.filter(([id, isCreator]) => id && isCreator)
-  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
-}
-handler.alias = ['owner']
-handler.command = /^(owner)$/i
+import fetch from "node-fetch";
+let handler = async (m, { conn, usedPrefix, text, args, command }) => {
+    let teks = "ها هو مالكي >\\<";
+    await conn.sendMessage(
+        m.chat,
+        {
+            text: teks,
+            mentions: [m.sender],
+            contextInfo: {
+                forwardingScore: 9999999,
+                isForwarded: false,
+                mentionedJid: [m.sender],
+                externalAdReply: {
+                    showAdAttribution: false,
+                    renderLargerThumbnail: true,
+                    title: "OMAR CHARAF",
+                    body: "يرجى عدم الإزعاج !!",
+                    containsAutoReply: true,
+                    mediaType: 1,
+                    thumbnailUrl: `https://telegra.ph/file/f3105b8337c9c66acd5bb.jpg`,
+                    mediaUrl: ``,
+                    sourceUrl: "https://wa.me/212670941551'",
+                },
+            },
+        },
+        { quoted: m },
+    );
+};
+handler.help = ["owner"];
+handler.tags = ["main"];
+handler.command = /^(owner|creator)$/i;
 
-export default handler
+export default handler;
