b1d945a06f0d84a792cf98726c2d4d891222042e
Omar <165955792+Omarcharaf1@users.noreply.github.com>
2024-04-13 11:12:39 +0000

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,17 +1,36 @@
-const { tiktokdl, tiktokdlv2 } = require('@bochilteam/scraper')
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
-    const { author: { nickname }, video, description } = await tiktokdl(args[0]).catch(async _ => await tiktokdlv2(args[0]))
-    const url = video.no_watermark || video.no_watermark_hd || video.with_watermark || video.no_watermark_raw
-    if (!url) throw 'Can\'t download video!'
-    conn.sendFile(m.chat, url, 'tiktok.mp4', `
-🔗 *Url:* ${url}
-🧏 *Nickname:* ${nickname}${description ? `🖹 *Description:* ${description}` : ''}
-`.trim(), m)
+import { tiktokdl } from '@bochilteam/scraper';
+import fg from 'api-dylux';
+
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {
+  
+ if (!args[0] && m.quoted && m.quoted.text) {
+  args[0] = m.quoted.text;
 }
-handler.help = ['tiktok'].map(v => v + ' <url>')
-handler.tags = ['downloader']
+if (!args[0] && !m.quoted) throw `Give the link of the video Tiktok or quote a tiktok link`;
+ if (!args[0].match(/tiktok/gi)) throw `Verify that the link is from TikTok`;
+ 
+ 
+  let txt = 'Here your Requested video';
 
-handler.command = /^(tik(tok)?(dl)?)$/i
+  try {
+    const { author: { nickname }, video, description } = await tiktokdl(args[0]);
+    const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd;
+    
+    if (!url) throw global.error;
+    
+    conn.sendFile(m.chat, url, 'tiktok.mp4', '', m);
+  } catch (err) {
+    try {
+      let p = await fg.tiktok(args[0]);
+      conn.sendFile(m.chat, p.play, 'tiktok.mp4', txt, m);
+    } catch {
+      m.reply('*An unexpected error occurred*');
+    }
+  }
+};
 
-module.exports = handler
+handler.help = ['tiktok'].map((v) => v + ' <url>');
+handler.tags = ['downloader'];
+handler.command = /^t(t|iktok(d(own(load(er)?)?|l))?|td(own(load(er)?)?|l))$/i;
+
+export default handler;
