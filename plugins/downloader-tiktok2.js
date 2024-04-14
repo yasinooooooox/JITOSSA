26462330871a10f49c07e806479e3fe3b46dbbb6
Omar <165955792+Omarcharaf1@users.noreply.github.com>
2024-04-14 17:24:09 +0100

    Update downloader-tiktok2.js

--- plugins/downloader-tiktok2.js
+++ plugins/downloader-tiktok2.js
@@ -1,46 +1,34 @@
-import {
-    tiktokdl
-} from '@bochilteam/scraper'
-import {
-    aiovideodl
-} from '../../lib/tiktokdl.js'
-import fetch from 'node-fetch'
+import { tiktokdl } from '@bochilteam/scraper';
+import fg from 'api-dylux';
 
-let handler = async (m, {
-    conn,
-    args,
-    usedPrefix,
-    command
-}) => {
-    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-    let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
-    let name = await conn.getName(who)
-    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {
+  if (!args[0] && m.quoted && m.quoted.text) {
+    args[0] = m.quoted.text;
+  }
+  if (!args[0] && !m.quoted) throw `أعطني رابط الفيديو من Tiktok أو قم بالرد على رابط Tiktok`;
+  if (!args[0].match(/tiktok/gi)) throw `تحقق من أن الرابط هو من TikTok`;
+ 
+  let txt = 'هنا فيديو الطلب الخاص بك';
+
+  try {
+    const { author: { nickname }, video, description } = await tiktokdl(args[0]);
+    const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd;
+    
+    if (!url) throw global.error;
+    
+    conn.sendFile(m.chat, url, 'tiktok.mp4', '', m);
+  } catch (err) {
     try {
-        const {
-            author: {
-                nickname
-            },
-            video,
-            description
-        } = await tiktokdl(args[0])
-        const url = video.no_watermark || video.no_watermark2 || video.no_watermark_raw
-        if (!url) throw 'Can\'t download video!'
-        let caption = `*Nickname:* ${nickname}
-*Description:* ${description}`
-        conn.sendFile(m.chat, url, '', caption, m)
+      let p = await fg.tiktok(args[0]);
+      conn.sendFile(m.chat, p.play, 'tiktok.mp4', '_تابعني على إنستجرام 🥰_ \n www.instagram.com/ovmar_1', m);
     } catch {
-        const {
-            res
-        } = await aiovideodl(args[0])
-        const urll = res.data.url
-        if (!urll) throw 'Can\'t download video!'
-        let caption = `*Nickname:* ${wm}`
-        conn.sendFile(m.chat, urll, '', caption, m)
+      m.reply('*حدث خطأ غير متوقع*');
     }
-}
-handler.help = ['tiktok2'].map(v => v + ' <url>')
-handler.tags = ['downloader']
-handler.command = /^(tiktok2)$/i
+  }
+};
 
-export default handler
+handler.help = ['tiktok2'].map((v) => v + ' <الرابط>');
+handler.tags = ['downloader'];
+handler.command = /^(tiktok2|tt2)(d(own(load(er)?)?|l|2))?$/i;
+handler.limit = false
+export default handler;
