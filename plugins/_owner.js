841dba7691a961c22b78f7cf2d537d6eb3857336
Omar <165955792+Omarcharaf1@users.noreply.github.com>
2024-04-14 21:47:33 +0100

    Update _owner.js

--- plugins/_owner.js
+++ plugins/_owner.js
@@ -1,33 +1,11 @@
-import fetch from "node-fetch";
-let handler = async (m, { conn, usedPrefix, text, args, command }) => {
-    let teks = "* *التواصل مع مطور البوت*\n\n *إنستجرام*\nwww.instagram.com/ovmar_1\n\n *واتساب*\n wa.me/212670941551\n\n *تيليجرام*\n t.me/Jinkx7 \n\n _®OMAR CHARAF_";
-    await conn.sendMessage(
-        m.chat,
-        {
-            text: teks,
-            mentions: [m.sender],
-            contextInfo: {
-                forwardingScore: 9999999,
-                isForwarded: false,
-                mentionedJid: [m.sender],
-                externalAdReply: {
-                    showAdAttribution: false,
-                    renderLargerThumbnail: true,
-                    title: "OMAR CHARAF",
-                    body: "يرجى عدم الإزعاج !!",
-                    containsAutoReply: true,
-                    mediaType: 1,
-                    thumbnailUrl: `https://telegra.ph/file/f3105b8337c9c66acd5bb.jpg`,
-                    mediaUrl: ``,
-                    sourceUrl: "https://wa.me/212670941551'",
-                },
-            },
-        },
-        { quoted: m },
-    );
-};
-handler.help = ["owner"];
-handler.tags = ["main"];
-handler.command = /^(owner|creator)$/i;
+function handler(m) {
+  const data = global.owner.filter(([id, isCreator]) => id && isCreator)
+  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
 
-export default handler;
+}
+
+handler.help = ['owner']
+handler.tags = ['owner']
+handler.command = ['owner', 'creator', 'creador', 'dueño', 'Gowner'] 
+
+export default handler
