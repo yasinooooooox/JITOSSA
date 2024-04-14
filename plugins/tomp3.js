819b1ac9168c6d306ffca6ba4261080af184ea40
Omar <165955792+Omarcharaf1@users.noreply.github.com>
2024-04-14 21:49:20 +0100

    Update tomp3.js

--- plugins/tomp3.js
+++ plugins/tomp3.js
@@ -1,18 +1,28 @@
-import { toAudio } from '../lib/converter.js'
+import { toAudio } from '../lib/converter.js';
 
 let handler = async (m, { conn, usedPrefix, command }) => {
-    let q = m.quoted ? m.quoted : m
-    let mime = (q || q.msg).mimetype || q.mediaType || ''
-    if (!/video|audio/.test(mime)) throw `Reply video/voice note you want to convert to audio/mp3 with command *${usedPrefix + command}*`
-    let media = await q.download()
-    if (!media) throw 'Can\'t download media'
-    let audio = await toAudio(media, 'mp4')
-    if (!audio.data) throw 'Can\'t convert media to audio'
-    conn.sendMessage(m.chat, { audio: audio.data,  mimetype: 'audio/mpeg' }, { quoted: m })
-}
-handler.help = ['tomp3']
-handler.tags = ['uploade']
-handler.alias = ['tomp3', 'toaudio']
-handler.command = /^to(mp3|audio)$/i
+    let q = m.quoted ? m.quoted : m;
+    let mime = (q || q.msg).mimetype || q.mediaType || '';
+    if (!/video|audio/.test(mime)) throw `رد برسالة صوتية أو فيديو تريد تحويلها إلى صوت/MP3 باستخدام الأمر *${usedPrefix + command}*`;
 
-export default handler
+    let waitMessage = await conn.sendMessage(m.chat, 'جاري تحويل الملف، يرجى الانتظار...', { quoted: m });
+
+    try {
+        let media = await q.download();
+        if (!media) throw 'لا يمكن تحميل الملف';
+        let audio = await toAudio(media, 'mp4');
+        if (!audio.data) throw 'لا يمكن تحويل الملف إلى صوت';
+        conn.sendMessage(m.chat, { audio: audio.data, mimetype: 'audio/mpeg' }, { quoted: m });
+    } catch (error) {
+        throw error; // تعامل مع الخطأ حسب احتياجاتك
+    } finally {
+        await conn.deleteMessage(m.chat, waitMessage.key);
+    }
+};
+
+handler.help = ['tomp3'];
+handler.tags = ['upload'];
+handler.alias = ['tomp3', 'toaudio'];
+handler.command = /^to(mp3|audio)$/i;
+
+export default handler;
