347a0ad67c941e1c48761aebd35efdd86032f7fb
Omar <165955792+Omarcharaf1@users.noreply.github.com>
2024-04-16 12:01:36 +0100

    Update owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,61 +1,14 @@
-import { exec } from 'child_process';
 
-let handler = async (m, { conn, usedPrefix, command, isOwner }) => {
-    
-    try {
-        const repoOwner = 'AOMAR-7'; // https://github.com/AOMAR-7/JITOSSA
-        const repoName = 'JITOSSA';
-        const branch = 'master'; //masterdefault
+import { execSync } from 'child_process'
+let handler = async (m, { conn, text }) => {
+if (conn.user.jid == conn.user.jid) {
+let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
+//require('fs').readdirSync('plugins').map(v=>global.reload('', v))
+conn.reply(m.chat, stdout.toString(), m)
+}}
+handler.help = ['update']
+handler.tags = ['owner']
+handler.command = ['update', 'actualizar', 'fix', 'fixed'] 
+handler.rowner = true
 
-        m.reply('Checking for updates...');
-
-        exec(`git ls-remote https://github.com/${repoOwner}/${repoName}.git ${branch}`, async (error, stdout, stderr) => {
-            if (error) {
-                console.error('Update check error:', error);
-                console.error('Update check stderr:', stderr);
-                await conn.reply(m.chat, 'جاري البحث عن تحديث.', m);
-                return;
-            }
-
-            const remoteCommit = stdout.trim();
-            const localCommit = require('child_process').execSync('git rev-parse HEAD').toString().trim();
-
-            if (remoteCommit === localCommit) {
-                await conn.reply(m.chat, '> Jessi-MD is up to date. No updates found.', m);
-            } else {
-                await conn.reply(m.chat, 'ثم الوصول إلىتحديث جديد من ق%& المطور جاري التحديث', m);
-
-                exec('git pull origin main', async (updateError, updateStdout, updateStderr) => {
-                    if (updateError) {
-                        console.error('> JITOSSA update error:', updateError);
-                        console.error('> JITOSSA update stderr:', updateStderr);
-                        await conn.reply(m.chat, '> JITOSSA update failed.', m);
-                        return;
-                    }
-
-                    await conn.reply(m.chat, '> JITOSSA ثم تحديث البوت.  إعادة تشغيل البوت مجددا', m);
-
-                    
-                    setTimeout(() => {
-                        conn.send('> Jessi-MD is restarting...');
-                        process.exit(0);
-                    }, 1000);
-                });
-            }
-        });
-    } catch (err) {
-        console.error('Update check error:', err);
-        await conn.reply(m.chat, 'Update check failed.', m);
-    }
-};
-
-handler.help = ['update];
-handler.tags = ['misc'];
-handler.command = /^(update)$/i;
-
-handler.owner = true
-handler.botAdmin = true
-
-
-
-export default handler;
+export default handler
