df89588b2ab2e5fcd7ce7be9258c8d5083085cc2
Omar <165955792+Omarcharaf1@users.noreply.github.com>
2024-04-16 19:25:51 +0100

    Delete plugins/tools-bingimg.js

--- plugins/tools-bingimg.js
+++
@@ -1,23 +0,0 @@
-import fetch from 'node-fetch'; // استيراد مكتبة node-fetch
-
-let handler = async (m, { conn, text }) => {
-    // التحقق من وجود نص
-    if (!text) throw '😇ما الذي تريدني أن أنشئه؟ \n\n مثال\n. bingimg2 women'
-    m.react(rwait) // تفعيل رد الفعل انتظار
-
-    // تشفير النص لاستخدامه في URL
-    let msg = encodeURIComponent(text)
-    let res = await fetch(`https://aemt.me/bingimg?text=${msg}`) // إرسال طلب HTTP للحصول على الصورة
-    let data = await res.json() // تحويل الرد إلى JSON
-    console.log(data)
-    let buffer = data.result // استخراج الصورة من البيانات الواردة
-    // إرسال الصورة كمرفق في الدردشة
-    conn.sendFile(m.chat, buffer, 'image.png', `_تابعني على إنستجرام 🥰_ \n www.instagram.com/ovmar_1`, m)
-    m.react(done) // تفعيل رد الفعل تم
-}
-
-handler.help = ['bingimg2 <query>'] // الأمر المساعد
-handler.tags = ['drawing'] // الوسم
-handler.command = /^bingimg2$/i // نمط الأمر المقبول
-
-export default handler // تصدير الدالة handler كافتراضي للاستخدام في أي مكان آخر
