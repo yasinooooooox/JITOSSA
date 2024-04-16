f4bb58ff25063f718e4c2add4328ab3ce8d2ca89
Omar <165955792+Omarcharaf1@users.noreply.github.com>
2024-04-16 10:58:02 +0100

    Update downloader-igstory.js

--- plugins/downloader-igstory.js
+++ plugins/downloader-igstory.js
@@ -1,56 +1,22 @@
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) throw `${lenguajeMY['smsAvisoMG']()}𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙀𝙇 𝙉𝙊𝙈𝘽𝙍𝙀 𝘿𝙀 𝙐𝙎𝙐𝘼𝙍𝙄𝙊 𝘿𝙀 𝙄𝙉𝙎𝙏𝘼𝙂𝙍𝘼𝙈 𝙋𝘼𝙍𝘼 𝘿𝙀𝙎𝘾𝘼𝙍𝙂𝘼𝙍 𝙇𝘼𝙎 𝙃𝙄𝙎𝙏𝙊𝙍𝙄𝘼𝙎\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} Wilson_cppjz*\n\n𝙀𝙉𝙏𝙀𝙍 𝙄𝙉𝙎𝙏𝘼𝙂𝙍𝘼𝙈 𝙐𝙎𝙀𝙍𝙉𝘼𝙈𝙀 𝙏𝙊 𝘿𝙊𝙒𝙉𝙇𝙊𝘼𝘿 𝙎𝙏𝙊𝙍𝙄𝙀𝙎\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix + command} Wilson_cppjz*`
-await m.reply(global.wait)    
-const res = await fetch(`https://api.lolhuman.xyz/api/igstory/${args[0]}?apikey=${lolkeysapi}`)
-var anu = await res.json()
-var anuku = anu.result
-if (anuku == '') return m.reply(`${lenguajeMY['smsAvisoFG']()}𝙐𝙎𝙐𝘼𝙍𝙄𝙊 𝙄𝙉𝙑𝘼́𝙇𝙄𝘿𝙊𝙎 𝙊 𝙎𝙄𝙉 𝙃𝙄𝙎𝙏𝙊𝙍𝙄𝘼𝙎`)  
-for (var i of anuku) {
-let res = await axios.head(i)
-let mime = res.headers['content-type']
-if (/image/.test(mime)) await conn.sendFile(m.chat, i, 'error.jpg', null, m).catch(() => { return m.reply(`${lenguajeMY['smsAvisoFG']()}𝙐𝙎𝙐𝘼𝙍𝙄𝙊 𝙄𝙉𝙑𝘼́𝙇𝙄𝘿𝙊𝙎 𝙊 𝙎𝙄𝙉 𝙃𝙄𝙎𝙏𝙊𝙍𝙄𝘼𝙎`)})
-if (/video/.test(mime)) await conn.sendFile(m.chat, i, 'error.mp4', null, m).catch(() => { return m.reply(`${lenguajeMY['smsAvisoFG']()}𝙐𝙎𝙐𝘼𝙍𝙄𝙊 𝙄𝙉𝙑𝘼́𝙇𝙄𝘿𝙊𝙎 𝙊 𝙎𝙄𝙉 𝙃𝙄𝙎𝙏𝙊𝙍𝙄𝘼𝙎`)})
-conn.reply(m.chat, `${lenguajeMY['smsAvisoIIG']()}💖 *Infórmate sobre las Novedades y recuerda tener la última versión.*\n\n💝 *Find out about what's new and remember to have the latest version.*`, m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
-title: author,
-body: 'Super Bot WhatsApp',         
-previewType: 0, thumbnail: fs.readFileSync("./media/menus/Menu3.jpg"),
-sourceUrl: `https://github.com/WilsonOFC/MylingBot-MD`}}})
-}}
-handler.help = ['igstory <username>']
-handler.tags = ['downloader']
-handler.command = ['igstory', 'ighistoria', 'ighistorias' ]
-handler.limit = 3
-handler.exp = 87
-export default handler
-
-
-/*import axios from 'axios'
-import hx from 'hxz-api' 
-
-let handler = async (m, { conn, args, usedPrefix, command, text}) => {
-if (!text) throw `${lenguajeMY['smsAvisoMG']()}𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙀𝙇 𝙉𝙊𝙈𝘽𝙍𝙀 𝘿𝙀 𝙐𝙎𝙐𝘼𝙍𝙄𝙊 𝘿𝙀 𝙄𝙉𝙎𝙏𝘼𝙂𝙍𝘼𝙈 𝙋𝘼𝙍𝘼 𝘿𝙀𝙎𝘾𝘼𝙍𝙂𝘼𝙍 𝙇𝘼𝙎 𝙃𝙄𝙎𝙏𝙊𝙍𝙄𝘼𝙎\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} Wilson_cppjz*\n\n𝙀𝙉𝙏𝙀𝙍 𝙄𝙉𝙎𝙏𝘼𝙂𝙍𝘼𝙈 𝙐𝙎𝙀𝙍𝙉𝘼𝙈𝙀 𝙏𝙊 𝘿𝙊𝙒𝙉𝙇𝙊𝘼𝘿 𝙎𝙏𝙊𝙍𝙄𝙀𝙎\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix + command} Wilson_cppjz*`
-
-hx.igstory(text).then(async (result) => {
-for (let i of result.medias) {
-if (i.url.includes("mp4")) {            
-conn.sendFile(m.chat, i.url, 'igstory.mp4', wm, m)
-} else {     
-conn.sendFile(m.chat, i.url, '', '', m)
-}}}); 
-  
-let info = `💖 *Infórmate sobre las Novedades y recuerda tener la última versión.*\n\n💝 *Find out about what's new and remember to have the latest version.*
-  `
- conn.sendButton(m.chat, info, `𝗠𝗬𝗟𝗜𝗡𝗚𝗕𝗢𝗧 - 𝘼𝙎𝙄𝙎𝙏𝙀𝙉𝘾𝙄𝘼\n${asistencia}\n\n`, [
-['𝙈𝙚𝙣𝙪 𝘿𝙚𝙨𝙘𝙖𝙧𝙜𝙖𝙨 🌀', '#descargasmenu'],
-['𝙈𝙚𝙣𝙪 𝘾𝙤𝙢𝙥𝙡𝙚𝙩𝙤 | 𝙁𝙪𝙡𝙡 𝙈𝙚𝙣𝙪 ✨', '.allmenu'],
-['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']], m)
-/*.trim()  
-await conn.sendHydrated(m.chat, info, wm, null, ig, '𝙄𝙣𝙨𝙩𝙖𝙜𝙧𝙖𝙢', null, null, [
-['𝙈𝙚𝙣𝙪 𝘿𝙚𝙨𝙘𝙖𝙧𝙜𝙖𝙨 🌀', '#descargasmenu'],
-['𝙈𝙚𝙣𝙪 𝘾𝙤𝙢𝙥𝙡𝙚𝙩𝙤 | 𝙁𝙪𝙡𝙡 𝙈𝙚𝙣𝙪 ✨', '.allmenu'],
-['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
-], m,)  
+    if (!args[0]) throw `${lenguajeMY['smsAvisoMG']()} *هاذا الأمر خاص بتحميل قصص الإنستجرام*\n\n*مثال الإستخدام*\n${usedPrefix + command}  ovmar_1`
+    await m.reply(global.wait)    
+    const res = await fetch(`https://api.lolhuman.xyz/api/igstory/${args[0]}?apikey=${lolkeysapi}`)
+    var anu = await res.json()
+    var anuku = anu.result
+    if (anuku == '') return m.reply(`${lenguajeMY['smsAvisoFG']()} *لايوجد أي قصة لهاذا الشخص*`)  
+    for (var i of anuku) {
+        let res = await axios.head(i)
+        let mime = res.headers['content-type']
+        if (/image/.test(mime)) await conn.sendFile(m.chat, i, 'error.jpg', null, m).catch(() => { return m.reply(`${lenguajeMY['smsAvisoFG']()} لاتوجد أي قصة هنا`)})
+        if (/video/.test(mime)) await conn.sendFile(m.chat, i, 'error.mp4', null, m).catch(() => { return m.reply(`${lenguajeMY['smsAvisoFG']()} لاتوجد أي قصة هنا`)})
+        conn.reply(m.chat, `${lenguajeMY['smsAvisoIIG']()} `_تابعني على إنستجرام 🥰_ \n www.instagram.com/ovmar_1`, m, {
+            contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
+            title: author,
+            body: 'Super Bot WhatsApp',         
+            previewType: 0, thumbnail: fs.readFileSync 'https://telegra.ph/file/c11ccb4fff461c40ab313.jpg',
+            sourceUrl: `https://github.com/omarcharaf1/JITOSSA`}}})
+    }
 }
 handler.help = ['igstory <username>']
 handler.tags = ['downloader']
@@ -58,4 +24,3 @@ handler.command = ['igstory', 'ighistoria', 'ighistorias' ]
 handler.limit = 3
 handler.exp = 87
 export default handler
-*/
