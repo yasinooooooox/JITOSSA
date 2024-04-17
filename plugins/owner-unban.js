708c63c8f3d24ab6a04831dab02d52f30a932706
Omar <165955792+Omarcharaf1@users.noreply.github.com>
2024-04-17 09:56:03 +0100

    Update owner-unban.js

--- plugins/owner-unban.js
+++ plugins/owner-unban.js
@@ -1,17 +1,22 @@
-let handler = async (m, { conn, text }) => {
-    if (!text) throw 'من ترغب في إلغاء حظره؟'
-    let who
-    if (m.isGroup) who = m.mentionedJid[0]
-    else who = m.chat
-    if (!who) throw 'الرجاء وضع علامة؟'
-    let users = global.db.data.users
-    users[who].banned = false
-    users[who].warning = 0
-    conn.reply(m.chat, '*تم إزالة الحظر من JITOSSA عنك*', m)
-}
-handler.help = ['unban']
-handler.tags = ['owner']
-handler.command = /^unban(user)?$/i
-handler.rowner = true
+//import db from '../lib/database.js'
 
-export default handler
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+    let who
+     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
+     else who = m.chat
+     let user = global.db.data.users[who]
+     if (!who) throw `*قم بالإشارة للشخص الذي تريد إزالة الحظر عنه*\\n مثال الإستخدام \n .unban @user`
+     let users = global.db.data.users
+     users[who].banned = false
+     conn.reply(m.chat, `
+*ثم إزالة الحظر بنجاح ✔️*
+ 
+ ───────────
+ @${who.split`@`[0]} *الأن يمكنك إستخدام البوت أسف* `, m, { mentions: [who] })
+ }
+ handler.help = ['un @user']
+ handler.tags = ['owner']
+ handler.command = /^unban$/i
+ handler.rowner = true
+ 
+ export default handler
