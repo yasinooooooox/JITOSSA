a0d98ecc13df4de0a348ce15f60fcb566a513409
Omar <165955792+Omarcharaf1@users.noreply.github.com>
2024-04-18 10:26:34 +0100

    Update info-scbot.js

--- plugins/info-scbot.js
+++ plugins/info-scbot.js
@@ -1,19 +1,24 @@
-import fetch from 'node-fetch'; // استيراد مكتبة fetch لإجراء طلبات HTTP
-import moment from 'moment-timezone'; // استيراد مكتبة moment-timezone للتعامل مع التواريخ والأوقات
+import { promises } from 'fs'
+import { join } from 'path'
 
-var handler = async (m, { conn, usedPrefix }) => {
-    let res = await fetch('https://api.github.com/repos/Omarcharaf1/JITOSSA');
-    let json = await res.json();
+let handler = async function (m, { conn, __dirname }) {
+let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
+  
+m.reply(`  
+*≡ السكريبت: JITOSSA*  
+*JITOSSA هو بوت وسكريبت API يتخصص في إرسال التطبيقات والأغاني ومقاطع الفيديو وغيرها من المحتويات المتعلقة بالهاتف وتطبيقاته.*  
 
-    let git = `*معلومات البوت*\n\n◦ *الاسم* : ${json.name}\n◦ *عدد الزوار* : ${json.watchers_count}\n◦ *الحجم* : ${(json.size / 1024).toFixed(2)} ميجابايت\n◦ *تاريخ التحديث* : ${moment(json.updated_at).format('DD/MM/YY - HH:mm:ss')}\n◦ *رابط المشروع* : ${json.html_url}\n\n	   ${json.forks_count} Forks · ${json.stargazers_count} Stars · ${json.open_issues_count} Issues\n\n${azami}`;
+*▢ يمكنك الحصول على السكريبت من هنا:* 
 
-    await conn.sendUrl(m.chat, git, m, { externalAdReply: { mediaType: 1, renderLargerThumbnail: true, thumbnail: imagen2, thumbnailUrl: imagen1, title: '\t\t\t\t\t\ _JITOSSA MD_', }});
+*[رابط السكريب]*
+https://www.github.com/Omarcharaf1/JITOSSA
+
+`.trim())
+    
 }
 
-handler.tags = ['info']; // وسم يدل على نوعية الأمر
-handler.help = ['owner']; // وصف الأمر
-handler.command = ['sc', 'script', 'codigo', 'git', 'github']; // الأوامر التي يتم استخدامها لاستدعاء الأمر
+handler.help = ['script']
+handler.tags = ['tools']
+handler.command = ['sc', 'git', 'script'] 
 
-handler.register = true; // تفعيل تسجيل الأمر ليظهر في قائمة المساعد
-
-export default handler; // تصدير الأمر
+export default handler
