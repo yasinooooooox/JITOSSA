830266d44df126547a704a3aa1ae4b28deb92833
Omar <165955792+Omarcharaf1@users.noreply.github.com>
2024-04-19 14:29:21 +0100

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,69 +1,48 @@
+import axios from 'axios'
 
-import axios from 'axios';
-import cheerio from 'cheerio';
+let isProcessing = false; // متغير للتحكم في التكرار
 
 var handler = async (m, { conn, args }) => {
-  if (!args[0]) {
-    throw 'أين الرابط الخاص بالفيديو Tiktok';
-  }
-
-  try {
-    await conn.reply(m.chat, 'جاري تنزيل الفيديو من سيرفراتنا \n _تابعني على إنستجرام 🥰_ \n www.instagram.com/ovmar_1', m);
-
-    const { thumbnail, video, audio } = await tiktokdl(args[0]);
-    const url = video;
-
-    if (!url) {
-      throw 'لايمكن تحميل الفيديو أسفة 😄';
+    if (isProcessing) {
+       // m.reply('انتظر قليلاً، يتم معالجة الطلب السابق...');
     }
+     m.reply('سوف يتم الارسال قريبا'),
+  
+    isProcessing = false; // تعيين المتغير للتكرار إلى true قبل تنفيذ الكود
 
-    await conn.sendMessage(m.chat, { video: { url: url } }, m);
-    await conn.reply(m.chat, '_تابعني على إنستجرام 🥰_ \n www.instagram.com/ovmar_1', m);
-  } catch (error) {
-    conn.reply(m.chat, `Error : ${error}`, m);
-  }
+    try {
+        if (!args[0]) {
+            throw 'يرجى إدخال رابط الفيديو.';
+        }
+
+     //   await m.react('✔️')
+
+        const tiktokData = await tryServer1(args[0]);
+
+        if (!tiktokData) {
+            throw 'فشل تنزيل الفيديو!';
+        }
+
+        const videoURL = tiktokData.video.noWatermark;
+
+        await conn.sendFile(m.chat, videoURL, 'tiktok.mp4', '', m);
+    } catch (error) {
+        throw `خطأ: ${error}`;
+    } finally {
+        isProcessing = false; // إعادة تعيين المتغير للتكرار إلى false بعد الانتهاء من الكود
+    }
 };
 
-handler.help = ['tiktok'].map((v) => v + ' <url>');
-handler.tags = ['downloader'];
-handler.command = /^t(t|iktok(d(own(load(er)?)?|l))?|td(own(load(er)?)?|l))$/i;
+handler.help = ['tiktok'].map((v) => v + ' <رابط>')
+handler.tags = ['downloader']
+handler.command = /^t(t|iktok(d(own(load(er)?)?|l))?|td(own(load(er)?)?|l))$/i
+handler.register = false
+handler.limit = true
 
-export default handler;
+export default handler
 
-async function tiktokdl(url) {
-  if (!/tiktok/.test(url)) {
-    throw 'الرابط غير صحيح حاول برابط جديد 😆';
-  }
-
-  const gettoken = await axios.get('https://tikdown.org/id');
-  const $ = cheerio.load(gettoken.data);
-  const token = $('#download-form > input[type=hidden]:nth-child(2)').attr('value');
-  const param = {
-    url: url,
-    _token: token,
-  };
-
-  const { data } = await axios.request('https://tikdown.org/getAjax?', {
-    method: 'post',
-    data: new URLSearchParams(Object.entries(param)),
-    headers: {
-      'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-      'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.115',
-    },
-  });
-
-  var getdata = cheerio.load(data.html);
-
-  if (data.status) {
-    return {
-      status: true,
-      thumbnail: getdata('img').attr('src'),
-      video: getdata('div.download-links > div:nth-child(1) > a').attr('href'),
-      audio: getdata('div.download-links > div:nth-child(2) > a').attr('href'),
-    };
-  } else {
-    return {
-      status: false,
-    };
-  }
+async function tryServer1(url) {
+    let tiklydownAPI = `https://api.tiklydown.eu.org/api/download?url=${url}`;
+    let response = await axios.get(tiklydownAPI);
+    return response.data;
 }
