8c496152a28ce3569e51a59a5f37f2599c311017
Omar <165955792+Omarcharaf1@users.noreply.github.com>
2024-04-21 10:30:14 +0100

    Update main.js

--- main.js
+++ main.js
@@ -330,7 +330,7 @@ global.reloadHandler = async function(restatConn) {
   }
 
   conn.welcome = '*مرحبا بك أيها العضو الجديد في المجموعة*, @user ! \n \n *معلومات البوت*\n\n group whatsapp https://chat.whatsapp.com/HcXWPVjjVkY5JIw88Xokg5 \n\n instagram \n www.instagram.com/ovmar_1\n\n> ＪＩＴＯＳＳＡ ＢＯＴ';
-  conn.bye = '*لقد غادر هاذا العضو قبل قليل*, @user \n \n *معلومات البوت*\n\n group whatsapp https://chat.whatsapp.com/HcXWPVjjVkY5JIw88Xokg5 \n\n instagram \n www.instagram.com/ovmar_1\n\n> ＪＩＴＯＳＳＡ ＢＯＴ';
+  conn.bye = '*لقد غادر ت يا @user المجموعة*\n \n *معلومات البوت*\n\n *group whatsapp*\n _https://chat.whatsapp.com/HcXWPVjjVkY5JIw88Xokg5_ \n\n *instagram* \n www.instagram.com/ovmar_1\n\n> ＪＩＴＯＳＳＡ ＢＯＴ';
 
 conn.spromote = '*[ ℹ️ ] @user ثم الترقية إلى مشرج للمجموعة.*';
   conn.sdemote = '*[ ℹ️ ] @user لم يعد هاذا الشخص مشرفا.*';
