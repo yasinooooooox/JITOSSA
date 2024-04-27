0a35094407bd795594e253c5dd19cb32c1365457
Omar <165955792+Omarcharaf1@users.noreply.github.com>
2024-04-27 10:23:49 +0100

    Update main.js

--- main.js
+++ main.js
@@ -491,7 +491,7 @@ setInterval(async () => {
   if (stopped === 'close' || !conn || !conn.user) return;
   const _uptime = process.uptime() * 1000;
   const uptime = clockString(_uptime);
-  const bio = `JITOSSA BOT [ ⏳ ] Uptime: ${uptime}`;
+  const bio = `ثم التطوير من طرف عمر / JITOSSA`;
   await conn.updateProfileStatus(bio).catch((_) => _);
 }, 60000);
 function clockString(ms) {
