c14a23cd32f66fa946ca2d0caa47c1f042605e2c
Omar <165955792+Omarcharaf1@users.noreply.github.com>
2024-04-28 13:14:23 +0100

    Update plugins

---
+++ plugins/cogans.js
@@ -0,0 +1,44 @@
+import fetch from 'node-fetch'
+let handler = async (m, {
+    text,
+    command,
+    usedPrefix,
+    conn
+}) => {
+    var list_input = [
+        "baekhyung",
+        "dohkyungsoo",
+        "huangzitao",
+        "jhope",
+        "jimin",
+        "jungkook",
+        "kimjondae",
+        "kimjong",
+        "kimminseok",
+        "kimnanjoon",
+        "kimseok",
+        "kimtaehyung",
+        "luhan",
+        "ohsehun",
+        "suga",
+        "wuyifan"
+    ]
+    var salah_input = "*قائمة الصور الموجودة اختر اسم واكتبه مه الأمر \n مثـال :*\n" + usedPrefix + command + " vietnam \n*[ Daftar cogans ]*\n\n" + await ArrClean(list_input)
+    if (!list_input.includes(text)) throw salah_input
+    try {
+        let res = 'https://api.zeeoneofc.my.id/api/cogan/' + text + '?apikey=dhmDlD5x'
+        m.reply(wait)
+        await conn.sendFile(m.chat, res, 'result', "Result Cogan: *" + text.toUpperCase() + "*", m)
+    } catch (e) {
+        throw eror
+    }
+}
+handler.help = ["cogans"]
+handler.tags = ['image-edit']
+handler.command = ["cogans"]
+
+export default handler
+
+function ArrClean(str) {
+    return str.map((v, index) => ++index + ". " + v).join('\r\n')
+}
