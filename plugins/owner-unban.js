472693802ebbd211ad5c03e1b2dc0c5c63a83068
Omar <165955792+Omarcharaf1@users.noreply.github.com>
2024-04-29 23:23:31 +0100

    Update owner-unban.js

--- plugins/owner-unban.js
+++ plugins/owner-unban.js
@@ -1,22 +1,16 @@
 //import db from '../lib/database.js'
 
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-    let who
-     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
-     else who = m.chat
-     let user = global.db.data.users[who]
-     if (!who) throw `*قم بالإشارة للشخص الذي تريد إزالة الحظر عنه*\\n مثال الإستخدام \n .unban @user`
-     let users = global.db.data.users
-     users[who].banned = false
-     conn.reply(m.chat, `
-*ثم إزالة الحظر بنجاح ✔️*
- 
- ───────────
- @${who.split`@`[0]} \n *الأن يمكنك إستخدام البوت أسفة* `, m, { mentions: [who] })
- }
- handler.help = ['un @user']
- handler.tags = ['owner']
- handler.command = /^unban$/i
- handler.rowner = true
- 
- export default handler
+let handler = async (m, { conn, isOwner, isAdmin, isROwner }) => {
+    if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
+    global.db.data.chats[m.chat].isBanned = true
+    m.reply('✅ تم تعطيل البوت في هذه المجموعة')
+}
+handler.help = ['banchat']
+handler.tags = ['owner']
+handler.command = ['banchat', 'حظر']
+
+handler.group = true
+handler.owner = true
+
+
+export default handler
