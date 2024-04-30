0c95790e70f0b84ceb482ada8c10555eac52e197
Omar <165955792+Omarcharaf1@users.noreply.github.com>
2024-04-30 21:53:32 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,59 @@
-var _0x4fb754=_0x31e2;(function(_0x5ce85e,_0x14b760){var _0x3b05e3=_0x31e2,_0x61c60c=_0x5ce85e();while(!![]){try{var _0x594411=parseInt(_0x3b05e3(0xbc))/0x1*(parseInt(_0x3b05e3(0x97))/0x2)+parseInt(_0x3b05e3(0xa0))/0x3+parseInt(_0x3b05e3(0xb0))/0x4*(parseInt(_0x3b05e3(0x91))/0x5)+parseInt(_0x3b05e3(0xa9))/0x6*(-parseInt(_0x3b05e3(0x9e))/0x7)+-parseInt(_0x3b05e3(0xa5))/0x8+-parseInt(_0x3b05e3(0xa6))/0x9+parseInt(_0x3b05e3(0x94))/0xa;if(_0x594411===_0x14b760)break;else _0x61c60c['push'](_0x61c60c['shift']());}catch(_0x1b74ed){_0x61c60c['push'](_0x61c60c['shift']());}}}(_0x5827,0x8b7bc));import{watchFile,unwatchFile}from'fs';function _0x31e2(_0x3aaf21,_0x3f9d4d){var _0x5827f4=_0x5827();return _0x31e2=function(_0x31e2da,_0x20cf0f){_0x31e2da=_0x31e2da-0x86;var _0x4f2178=_0x5827f4[_0x31e2da];return _0x4f2178;},_0x31e2(_0x3aaf21,_0x3f9d4d);}import _0x5a8142 from'chalk';import{fileURLToPath}from'url';import _0x40cde8 from'fs';import _0x1f7af8 from'cheerio';import _0x1595ae from'node-fetch';function _0x5827(){var _0x52ebd5=['9523908GVksaQ','waittt','owner','594JZroEx','pdoc','>\x20سوف\x20يتم\x20رسم\x20صورتك\x20بعد\x20قليل\x20...','imagen3','año','imagen1','application/vnd.ms-excel','348hKBhLb','./src/Pre\x20Bot\x20Publi.png','titulowm','./src/nuevobot.jpg','212670941551','imagen4','imagen5','application/pdf','imagen2','suittag','waitt','>\x20_جاري\x20معالجة\x20طلبك\x20حاول\x20الإنتظار\x20..._\x0a\x0a\x20*متابعة\x20للمطور\x20في\x20إنستجرام*\x20\x0a*_instagram.com/ovmar_1_*','2LdmisQ','fecha','packname','titulowm2','application/msword','./src/+18.jpg','https://instagram.com/ovmar_1','./Menu3.png','mysticbot','application/vnd.openxmlformats-officedocument.presentationml.presentation','text/rtf','tiempo','wait','62735jdBTpv','toLocaleString','done','13071740GuKyMw','>\x20سوف\x20أجيب\x20عن\x20سؤالك\x20بعد\x20قليل\x20...','OMAR','119546OMWoJg','toLocaleDateString','\x0aJITOSSA','numeric','readFileSync','mes','JITOSSA','31262KjDlZd','author','718158nCmPdJ','./Menu.png','long','Omar','prems','5488200qyvEjJ'];_0x5827=function(){return _0x52ebd5;};return _0x5827();}import _0x5933b8 from'axios';import _0x27c04e from'moment-timezone';global['botnumber']='',global[_0x4fb754(0xa8)]=[[_0x4fb754(0xb4),_0x4fb754(0xa3),!![]]],global[_0x4fb754(0xb9)]=[_0x4fb754(0xb4)],global[_0x4fb754(0xa4)]=[_0x4fb754(0xb4)],global[_0x4fb754(0x86)]=_0x4fb754(0x9d),global[_0x4fb754(0x9f)]='',global['wm']=_0x4fb754(0x96),global[_0x4fb754(0xb2)]=_0x4fb754(0x9d),global[_0x4fb754(0x87)]='JITOSSA',global['igfg']=_0x4fb754(0x9d),global[_0x4fb754(0x90)]=_0x4fb754(0xbb),global[_0x4fb754(0xba)]=_0x4fb754(0x95),global[_0x4fb754(0xa7)]=_0x4fb754(0xab),global['rwait']='⏳',global[_0x4fb754(0x93)]='✅',global[_0x4fb754(0xae)]=_0x40cde8[_0x4fb754(0x9b)]('./Menu2.jpg'),global[_0x4fb754(0xb8)]=_0x40cde8[_0x4fb754(0x9b)](_0x4fb754(0xb3)),global[_0x4fb754(0xac)]=_0x40cde8[_0x4fb754(0x9b)](_0x4fb754(0xb1)),global[_0x4fb754(0xb5)]=_0x40cde8[_0x4fb754(0x9b)](_0x4fb754(0xa1)),global[_0x4fb754(0xb6)]=_0x40cde8[_0x4fb754(0x9b)](_0x4fb754(0x89)),global['imagen6']=_0x40cde8[_0x4fb754(0x9b)](_0x4fb754(0x8b)),global['mods']=[],global['d']=new Date(new Date()+0x36ee80),global['locale']='en',global['dia']=d[_0x4fb754(0x98)](locale,{'weekday':_0x4fb754(0xa2)}),global[_0x4fb754(0xbd)]=d['toLocaleDateString']('en',{'day':'numeric','month':_0x4fb754(0x9a),'year':'numeric'}),global[_0x4fb754(0x9c)]=d['toLocaleDateString']('en',{'month':_0x4fb754(0xa2)}),global[_0x4fb754(0xad)]=d['toLocaleDateString']('en',{'year':'numeric'}),global[_0x4fb754(0x8f)]=d[_0x4fb754(0x92)]('en-US',{'hour':_0x4fb754(0x9a),'minute':_0x4fb754(0x9a),'second':_0x4fb754(0x9a),'hour12':!![]}),global['wm2']=dia+'\x20'+fecha+_0x4fb754(0x99),global['gt']='JITOSSA',global[_0x4fb754(0x8c)]=_0x4fb754(0x9d),global['md']=_0x4fb754(0x8a),global[_0x4fb754(0x8c)]=_0x4fb754(0x8a),global['nomorown']=_0x4fb754(0xb4),global[_0x4fb754(0xaa)]=[_0x4fb754(0x8d),'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet','application/vnd.openxmlformats-officedocument.wordprocessingml.document',_0x4fb754(0xaf),_0x4fb754(0x88),_0x4fb754(0xb7),_0x4fb754(0x8e)];
-vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/msword', 'application/pdf', 'text/rtf'];
+import {watchFile, unwatchFile} from 'fs';
+import chalk from 'chalk';
+import {fileURLToPath} from 'url';
+import fs from 'fs'; 
+import cheerio from 'cheerio';
+import fetch from 'node-fetch';
+import axios from 'axios';
+import moment from 'moment-timezone';
+
+global.botnumber = ""
+//يمكنك إضافة رقمك للتحكم في البوت أو إن طلب شخص رقمك من البوت
+global.owner = [
+  ['212670941551', 'Omar', true],
+  
+];
+
+global.suittag = ['212670941551'];
+global.prems = ['212670941551'];
+
+global.packname = 'JITOSSA';
+global.author = '';
+global.wm = 'OMAR';
+global.titulowm = 'JITOSSA';
+global.titulowm2 = `JITOSSA`
+global.igfg = 'JITOSSA';
+global.wait = '> _جاري معالجة طلبك حاول الإنتظار ..._\n\n *متابعة للمطور في إنستجرام* \n*_instagram.com/ovmar_1_*';
+global.waitt = '> سوف أجيب عن سؤالك بعد قليل ...';
+global.waittt = '> سوف يتم رسم صورتك بعد قليل ...';
+global.rwait = '⏳';
+global.done = '✅';
+
+global.imagen1 = fs.readFileSync('./Menu2.jpg');
+global.imagen2 = fs.readFileSync('./src/nuevobot.jpg');
+global.imagen3 = fs.readFileSync('./src/Pre Bot Publi.png');
+global.imagen4 = fs.readFileSync('./Menu.png');
+global.imagen5 = fs.readFileSync('./src/+18.jpg');
+global.imagen6 = fs.readFileSync('./Menu3.png');
+
+global.mods = [];
+
+//* *******Tiempo***************
+global.d = new Date(new Date + 3600000);
+global.locale = 'en';
+global.dia = d.toLocaleDateString(locale, {weekday: 'long'});
+global.fecha = d.toLocaleDateString('en', {day: 'numeric', month: 'numeric', year: 'numeric'});
+global.mes = d.toLocaleDateString('en', {month: 'long'});
+global.año = d.toLocaleDateString('en', {year: 'numeric'});
+global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
+//* ****************************
+global.wm2 = `${dia} ${fecha}\nJITOSSA`;
+global.gt = 'JITOSSA';
+global.mysticbot = 'JITOSSA';
+global.md = 'https://instagram.com/ovmar_1';
+global.mysticbot = 'https://instagram.com/ovmar_1';
+global.nomorown = '212670941551';
+global.pdoc = ['application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/msword', 'application/pdf', 'text/rtf'];
 global.cmenut = '❖––––––『';
 global.cmenub = '┊✦ ';
 global.cmenuf = '╰━═┅═━––––––๑\n';
